481852a0a761c6ec1cf50e7649a9d7e839ed0f47
vaishnavi-puttur <83641912+vaishnavi-puttur@users.noreply.github.com>
2023-08-30 13:09:56 +0530

    Uploaded

---
+++ coonection.php
@@ -0,0 +1,12 @@
+<?php
+
+     $servername="localhost";
+     $username="root";
+     $password="";
+     $db_name="hotel reservation";
+     $conn=new mysqli($servername,$username,$password,$db_name);
+     if($conn->connect_error){
+         die("connection failed".$conn->connect_error);
+        }
+       echo "";
+?>
